ce59af5f8152fdba19e2ddda8351aacacab0b8e7
Mipsmonsta <yaojintham@gmail.com>
2021-03-16 05:11:46 +0800

    Added safe logo; page window unresizable; create nested vault folder if none

--- ui.py
+++ ui.py
@@ -7,32 +7,31 @@ from pageBrain import PageBrain
 from cryptography.fernet import InvalidToken
 from os import path
 import shutil
-            
+from pathlib import Path
 
 import random
-import pyperclip # third party library
+import pyperclip  # third party library
 
 #------------------------- Constants -----------------------------#
 # for password generation
 LETTERS = list("abcdefghijklmnopqrstuvwxyz")
 NUMBERS = list("0123456789")
 SYMBOLS = list("!#$%&()*+")
-        
+
 
 class MainWindow:
     def __init__(self):
-        
+
         self.window = Tk()
-        #self.window.geometry("400x400+100+100")
+        # self.window.geometry("400x400+100+100")
         self.window.minsize(width=400, height=350)
-       
+
         self.window.grid_columnconfigure(0, weight=1)
         self.window.grid_rowconfigure(0, weight=1)
 
-        
         self.window.title("Password Manager")
         self.window.config(padx=2, bg="white")
-        
+
         # menu
         menubar = Menu(self.window, bg="white")
         filemenu = Menu(menubar, tearoff=False)
@@ -43,60 +42,85 @@ class MainWindow:
         filemenu.add_command(label="Exit", command=self.window.quit)
         menubar.add_cascade(label="File", menu=filemenu)
         self.window.config(menu=menubar)
-        
+
         # frame
-        self.currFrame = None
-    
-        self.window.mainloop() 
-        
+        self.currFrame = SimpleFromIconFrame()
+        self.currFrame.grid(row=0, column=0)
+
+        self._createVaultFolder()
+
+        self.window.mainloop()
+
+    def _createVaultFolder(self):
+        folderPath = "./vault"
+        Path(folderPath).mkdir(parents=True, exist_ok=True)
+
     def openFrameWithPage(self):
         defaultPageName = "./vault/data.json"
         pageName = defaultPageName
-        fileName = askopenfilename(initialdir="./vault", 
+        fileName = askopenfilename(initialdir="./vault",
                                    filetypes=(("JSON Files", ".json"),))
         if fileName:
             pageName = fileName
         else:
-            messagebox.showinfo(title="No file selected", message="Default page file will be used")
-        
+            messagebox.showinfo(title="No file selected",
+                                message="Default page file will be used")
+
         pageBrain = PageBrain(pageName)
         if self.currFrame:
             self.currFrame.destroy()
         self.currFrame = FormFrame(self.window, pageBrain)
         self.currFrame.grid(row=0, column=0)
+        self.window.resizable(False, False)
 
     def renameVaultFile(self):
         if self.currFrame == None:
-            messagebox.showerror(title="No page opened", message="Cannot save!")
+            messagebox.showerror(title="No page opened",
+                                 message="Cannot save!")
             return
-        
-        fileName = asksaveasfilename(initialdir="./vault", 
+
+        fileName = asksaveasfilename(initialdir="./vault",
                                      title="Save Page File As",
                                      filetypes=(("JSON Files", ".json"),))
-        
+
         if fileName:
             if not fileName.endswith(".json"):
                 fileName = f"{fileName}.json"
-                
+
         dest = path.normpath(fileName)
-        source = path.normpath(path.abspath(self.currFrame.pageBrain.fullFileName))
-        if dest != source: # not the same
+        source = path.normpath(path.abspath(
+            self.currFrame.pageBrain.fullFileName))
+        if dest != source:  # not the same
             try:
                 shutil.move(source, dest)
             except FileNotFoundError:
-                messagebox.showerror(title="File not saved", message="Error in moving file")
+                messagebox.showerror(title="File not saved",
+                                     message="Error in moving file")
             else:
                 self.currFrame.pageBrain.fullFileName = dest
-                
-                        
+
+
+class SimpleFromIconFrame(Frame):
+    def __init__(self):
+        Frame.__init__(self, bg="white")
+
+        self.canvas = Canvas(self, width=330, height=330,
+                             bg="white", highlightthickness=0)
+
+        self.logoImage = PhotoImage(file="./safe.png")
+        self.canvas.create_image(165, 165, image=self.logoImage)
+        self.canvas.grid(row=0, column=0)
+
+
 class FormFrame(Frame):
     def __init__(self, parent: Tk, page: PageBrain):
         Frame.__init__(self, parent, bg="white")
 
         self.pageBrain = page
-        
-        self.canvas = Canvas(self, width=200, height=200, bg="white", highlightthickness=0)
-        
+
+        self.canvas = Canvas(self, width=200, height=200,
+                             bg="white", highlightthickness=0)
+
         self.logoImage = PhotoImage(file="./logo.png")
         self.canvas.create_image(100, 100, image=self.logoImage)
         self.canvas.grid(column=1, row=0)
@@ -112,13 +136,15 @@ class FormFrame(Frame):
         self.passwordLabel.grid(column=0, row=3)
 
         # status bar using a label
-        self.statusLabel = Label(self, text="", fg="grey", bg="white", relief=SUNKEN, anchor=E, width=60)
-        self.statusLabel.grid(column=0, row=5, sticky=W+E, columnspan=3, pady=(50, 2))
-
+        self.statusLabel = Label(
+            self, text="", fg="grey", bg="white", relief=SUNKEN, anchor=E, width=60)
+        self.statusLabel.grid(column=0, row=5, sticky=W+E,
+                              columnspan=3, pady=(50, 2))
 
         # entries
-        self.website = StringVar() # using string val so that can monitor it
-        self.website.trace_add("write", self.websiteChangedFindWord) # track the string var
+        self.website = StringVar()  # using string val so that can monitor it
+        # track the string var
+        self.website.trace_add("write", self.websiteChangedFindWord)
         self.websiteEntry = Entry(self, width=18, textvariable=self.website)
         self.websiteEntry.grid(column=1, row=1)
         self.websiteEntry.focus()
@@ -130,7 +156,8 @@ class FormFrame(Frame):
         self.passwordEntry.grid(column=1, row=3)
 
         # buttons
-        self.genPassButton = Button(self, text="Generate Password", bg="white", width=15)
+        self.genPassButton = Button(
+            self, text="Generate Password", bg="white", width=15)
         self.genPassButton.config(command=self.generatePassword)
         self.genPassButton.grid(column=2, row=3, sticky=W)
 
@@ -141,33 +168,34 @@ class FormFrame(Frame):
         self.searchButton = Button(self, text="Search", bg="white", width=15)
         self.searchButton.config(command=self.searchPassword)
         self.searchButton.grid(column=2, row=1, sticky=W)
-        
+
         # after 1 sec, ask user for password for page
         parent.after(1000, self.setMasterPassword)
-      
+
     def setMasterPassword(self):
         if self.pageBrain.masterPassword == None:
-            masterpw = askstring(title="Session Password", prompt="Enter Password")
+            masterpw = askstring(title="Session Password",
+                                 prompt="Enter Password")
             if len(masterpw):
-                self.pageBrain.masterPassword = masterpw  
-        
+                self.pageBrain.masterPassword = masterpw
+
     def generatePassword(self):
-        num_letters = random.randint(8, 10) # 8 to 10 letters
+        num_letters = random.randint(8, 10)  # 8 to 10 letters
         num_symbols = random.randint(2, 4)  # 2 to 4 symbols
         num_numbers = random.randint(2, 4)  # 2 to 4 numbers
         chosenLetters = random.choices(LETTERS, k=num_letters)
         chosenSymbols = random.choices(SYMBOLS, k=num_symbols)
         chosenNumbers = random.choices(NUMBERS, k=num_numbers)
-        
+
         password = chosenLetters + chosenSymbols + chosenNumbers
         random.shuffle(password)
-        
+
         genPasswordStr = "".join(password)
         self.passwordEntry.delete(0, END)
         self.passwordEntry.insert(0, genPasswordStr)
-    
+
     def save(self):
-        
+
         websiteStr = self.website.get().strip()
         emailUserStr = self.emailUserEntry.get().strip()
         passwordStr = self.passwordEntry.get().strip()
@@ -177,51 +205,54 @@ class FormFrame(Frame):
             messagebox.showinfo(title="Oops", message=msg, icon="warning")
             return
 
-        is_okay = messagebox.askokcancel(title=websiteStr, message=f"These are the details:\nEmail: {emailUserStr}\nPassword: {passwordStr}\nIs it okay to save?")
-            
+        is_okay = messagebox.askokcancel(
+            title=websiteStr, message=f"These are the details:\nEmail: {emailUserStr}\nPassword: {passwordStr}\nIs it okay to save?")
+
         if is_okay:
             self.pageBrain.saveToFile(websiteStr, emailUserStr, passwordStr)
-            
+
             # clear fields after saving
             self.website.set("")
-            self.emailUserEntry.delete(0,END)
+            self.emailUserEntry.delete(0, END)
             self.passwordEntry.delete(0, END)
 
     def searchPassword(self):
         searchWord = self.website.get()
         if len(searchWord) == 0:
             return
-        
+
         # Read json
         trimmedSearchWord = searchWord.strip()
-            
+
         if trimmedSearchWord in self.pageBrain.jsonDict['data']:
             userEmailCipher = self.pageBrain.jsonDict['data'][trimmedSearchWord]["emailUser"]
             passwordCipher = self.pageBrain.jsonDict['data'][trimmedSearchWord]['password']
-            
-            try:    
+
+            try:
                 userEmailPlain = self.pageBrain.decrypt(userEmailCipher)
                 passwordPlain = self.pageBrain.decrypt(passwordCipher)
             except InvalidToken:
-                #re-ask for session password
-                masterpw = askstring(title="Wrong Session Password", prompt="Enter Password")
+                # re-ask for session password
+                masterpw = askstring(
+                    title="Wrong Session Password", prompt="Enter Password")
                 if len(masterpw):
                     self.pageBrain.masterPassword = masterpw
             else:
                 msg = f"Email/User: {userEmailPlain}\nPassword: {passwordPlain}"
-                messagebox.showinfo(title=trimmedSearchWord, message=msg, icon="info")
+                messagebox.showinfo(title=trimmedSearchWord,
+                                    message=msg, icon="info")
                 pyperclip.copy(passwordPlain)
-    
+
     def websiteChangedFindWord(self, *args):
-       
+
         websiteStr = self.website.get().strip()
-        
+
         if len(websiteStr) == 0:
             self.statusLabel.config(text="")
             return
-        
+
         t = self.pageBrain.trie
-        
+
         for chr in websiteStr:
             if chr in t:
                 t = t[chr]
@@ -231,11 +262,9 @@ class FormFrame(Frame):
         foundWords = []
         countLimit = 3
         self._searchWordsWithPrefix(t, websiteStr, foundWords, countLimit)
-       
+
         self.statusLabel.config(text=" | ".join(foundWords))
-     
-    
-    
+
     def _searchWordsWithPrefix(self, prefixedTrie, suffix, foundwords, countLimit):
         if '#' in prefixedTrie:
             foundwords.append(suffix)
@@ -243,7 +272,7 @@ class FormFrame(Frame):
                 return
 
         for chr in (set(prefixedTrie.keys()) - set(['#'])):
-            self._searchWordsWithPrefix(prefixedTrie[chr], suffix+chr, foundwords, countLimit)
+            self._searchWordsWithPrefix(
+                prefixedTrie[chr], suffix+chr, foundwords, countLimit)
             if countLimit == len(foundwords):
-                break   
-   
+                break
